fe4cce2f6f724b347d1fbb6c0984012a23186702
PreferSentinel <ddas2okbesz@gmail.com>
2026-01-11 00:40:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ app.on('window-all-closed', () => {
 ipcMain.on('sentinel:start-chat', async (event, userMessage) => {
     try {
       const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash-latest",  // ← "-latest" eklendi
+    model: "gemini-1.5-flash",  // ← "-latest" eklendi
     systemInstruction: "Sen Sentinel'sin. Kullanıcıya her konuda yardımcı olan, çok samimi, kanka gibi konuşan ve zeki bir asistansın. Teknik terimler yerine günlük bir dil kullan, şakacı ve dost canlısı davran."
 });
 
@@ -57,5 +57,6 @@ ipcMain.on('sentinel:start-chat', async (event, userMessage) => {
     }
 
 });
+
 
 
